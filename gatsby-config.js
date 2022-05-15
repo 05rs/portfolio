6207a7d515770db4f57fd37a840e4c5ed728f553
raghavsethi <raghav.sethi@gobolt.co.in>
2022-05-15 15:29:27 +0530

    setup github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
 const config = require('./src/config');
 
 module.exports = {
+  pathPrefix: '/portfolio',
   siteMetadata: {
     title: 'Raghav Sethi',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    description: 'Raghav Sethi is a software engineer.',
     siteUrl: 'https://raghavsethi.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
