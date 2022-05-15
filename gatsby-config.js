6918afb4e399c675c3453babe3fcb9de4aec6bf3
raghavsethi <raghav.sethi@gobolt.co.in>
2022-05-15 16:18:49 +0530

    fixed description in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,10 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Raghav Sethi',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    description: 'Raghav Sethi is a software engineer.',
     siteUrl: 'https://raghavsethi.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    image: '', // Path to your image you placed in the 'static' folder
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
